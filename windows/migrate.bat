3d48e53789736fb2b8b766b520df6241021e66b2
Alex Crome <afscrome@users.noreply.github.com>
2015-08-21 13:12:34 +0100

    Find sh.exe when Git 2.5 for windows is installed. Fixes #112

--- windows/migrate.bat
+++ windows/migrate.bat
@@ -1,4 +1,15 @@
 :: Batch file that installer can run. Simply runs bash script with the same name
-
 cd %~dp0
-"C:\Program Files (x86)\Git\bin\sh.exe" --login -i migrate.sh
+
+:: Add known locations of Git to path to try and find sh.exe
+set PATH=%PATH%;%ProgramFiles%\git\bin\;%ProgramFiles(x86)%\Git\bin\
+
+where sh.exe 
+if ERRORLEVEL 1 (
+    echo "Could not find sh.exe"
+    exit 1234
+)
+
+sh.exe --login -i migrate.sh  
+
+:end
