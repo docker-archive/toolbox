efe05afdc956eedc571e552601a3b75b86a2cace
Markos Rendell <markosrendell@gmail.com>
2016-02-21 15:52:54 +0530

    Allowing you to pass in a machine name and use this for more than just boxes called default

--- windows/start.sh
+++ windows/start.sh
@@ -2,7 +2,7 @@
 
 trap '[ "$?" -eq 0 ] || read -p "Looks like something went wrong... Press any key to continue..."' EXIT
 
-VM=default
+VM=${1-default}
 DOCKER_MACHINE=./docker-machine.exe
 
 if [ ! -z "$VBOX_MSI_INSTALL_PATH" ]; then
