4dcef8f76b248d9422ff27f323c87375b7e1ccf5
Tim Birkett <tim@birkett-bros.com>
2016-04-01 21:55:22 +0100

    Fix failing variable tests and use elif to allow override of system HTTP_PROXY env variable by using DOCKER_HTTP_PROXY env variable

--- windows/start.sh
+++ windows/start.sh
@@ -36,16 +36,26 @@ STEP="Checking if machine $VM exists"
 if [ $VM_EXISTS_CODE -eq 1 ]; then
   "${DOCKER_MACHINE}" rm -f "${VM}" &> /dev/null || :
   rm -rf ~/.docker/machine/machines/"${VM}"
+
   #set proxy variables if they exists
-  if [ -n ${DOCKER_HTTP_PROXY+x} ]; then
-	PROXY_ENV="$PROXY_ENV --engine-env HTTP_PROXY=$DOCKER_HTTP_PROXY"
+  if [ -n "${DOCKER_HTTP_PROXY}" ]; then
+  PROXY_ENV="$PROXY_ENV --engine-env HTTP_PROXY=$DOCKER_HTTP_PROXY"
+  elif [ -n "${HTTP_PROXY}" ]; then
+  PROXY_ENV="$PROXY_ENV --engine-env HTTP_PROXY=$HTTP_PROXY"
   fi
-  if [ -n ${DOCKER_HTTPS_PROXY+x} ]; then
+
+  if [ -n "${DOCKER_HTTPS_PROXY}" ]; then
 	PROXY_ENV="$PROXY_ENV --engine-env HTTPS_PROXY=$DOCKER_HTTPS_PROXY"
+  elif [ -n "${HTTPS_PROXY}" ]; then
+  PROXY_ENV="$PROXY_ENV --engine-env HTTP_PROXY=$HTTPS_PROXY"
   fi
-  if [ -n ${DOCKER_NO_PROXY+x} ]; then
+
+  if [ -n "${DOCKER_NO_PROXY}" ]; then
 	PROXY_ENV="$PROXY_ENV --engine-env NO_PROXY=$DOCKER_NO_PROXY"
-  fi  
+  elif [ -n "${NO_PROXY}" ]; then
+	PROXY_ENV="$PROXY_ENV --engine-env NO_PROXY=$NO_PROXY"
+  fi
+
   "${DOCKER_MACHINE}" create -d virtualbox $PROXY_ENV "${VM}"
 fi
 
