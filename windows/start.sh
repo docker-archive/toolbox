b886c6bbde253606f4d1009acf81107c9fb2684b
Chisomo Sakala <chisomo.sakala@mombo.solution>
2016-10-03 06:46:28 -0500

    Improved start.sh to make it more robust.
    -Removed hard coded reference "/c/Program Files/Docker Toolbox" replacing it with $DOCKER_TOOLBOX_INSTALL_PATH
    -Added quotes around string variables that refer to things such as paths that could potentially have spaces after expansion
    -Permanently set environment variables DOCKER_HOST, DOCKER_CERT_PATH, DOCKER_MACHINE_NAME, DOCKER_TLS_VERIFY etc using windows SETX instead of the default SET that's provided by `docker-machine env` .
    
    Signed-off-by: Chisomo Sakala <chisomo.sakala@mombo.solution>

--- windows/start.sh
+++ windows/start.sh
@@ -2,12 +2,21 @@
 
 trap '[ "$?" -eq 0 ] || read -p "Looks like something went wrong in step ´$STEP´... Press any key to continue..."' EXIT
 
-# TODO: I'm sure this is not very robust.  But, it is needed for now to ensure
-# that binaries provided by Docker Toolbox over-ride binaries provided by
+#Quick Hack: used to convert e.g. "C:\Program Files\Docker Toolbox" to "/c/Program Files/Docker Toolbox"
+win_to_unix_path(){ 
+	wd="$(pwd)"
+	cd "$1"
+		the_path="$(pwd)"
+	cd "$wd"
+	echo $the_path
+}
+
+# This is needed  to ensure that binaries provided
+# by Docker Toolbox over-ride binaries provided by
 # Docker for Windows when launching using the Quickstart.
-export PATH="/c/Program Files/Docker Toolbox:$PATH"
+export PATH="$(win_to_unix_path "${DOCKER_TOOLBOX_INSTALL_PATH}"):$PATH"
 VM=${DOCKER_MACHINE_NAME-default}
-DOCKER_MACHINE=./docker-machine.exe
+DOCKER_MACHINE="${DOCKER_TOOLBOX_INSTALL_PATH}\docker-machine.exe"
 
 STEP="Looking for vboxmanage.exe"
 if [ ! -z "$VBOX_MSI_INSTALL_PATH" ]; then
@@ -61,14 +70,15 @@ if [ $VM_EXISTS_CODE -eq 1 ]; then
 fi
 
 STEP="Checking status on $VM"
-VM_STATUS="$( set +e ; ${DOCKER_MACHINE} status ${VM} )"
+VM_STATUS="$( set +e ; "${DOCKER_MACHINE}" status "${VM}" )"
 if [ "${VM_STATUS}" != "Running" ]; then
   "${DOCKER_MACHINE}" start "${VM}"
   yes | "${DOCKER_MACHINE}" regenerate-certs "${VM}"
 fi
 
 STEP="Setting env"
-eval "$(${DOCKER_MACHINE} env --shell=bash --no-proxy ${VM})"
+eval "$("${DOCKER_MACHINE}" env --shell=bash --no-proxy "${VM}" | sed -e "s/export/SETX/g" | sed -e "s/=/ /g")" &> /dev/null #for persistent Environment Variables, available in next sessions
+eval "$("${DOCKER_MACHINE}" env --shell=bash --no-proxy "${VM}")" #for transient Environment Variables, available in current session
 
 STEP="Finalize"
 clear
@@ -85,10 +95,11 @@ cat << EOF
               \____\_______/
 
 EOF
-echo -e "${BLUE}docker${NC} is configured to use the ${GREEN}${VM}${NC} machine with IP ${GREEN}$(${DOCKER_MACHINE} ip ${VM})${NC}"
+echo -e "${BLUE}docker${NC} is configured to use the ${GREEN}${VM}${NC} machine with IP ${GREEN}$("${DOCKER_MACHINE}" ip ${VM})${NC}"
 echo "For help getting started, check out the docs at https://docs.docker.com"
 echo
-cd
+echo 
+#cd #Bad: working dir should be whatever directory was invoked from rather than fixed to the Home folder
 
 docker () {
   MSYS_NO_PATHCONV=1 docker.exe "$@"
