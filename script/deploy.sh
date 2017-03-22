2aa365c91c4c3ecb146620e392e4a6a39130be4d
Nathan LeClaire <nathan.leclaire@gmail.com>
2017-03-22 15:25:44 -0700

    Be more strict in deploy automation
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Try several times to do the build (sometimes network or other issues causes
 # it to fail)
 for i in $(seq 0 4); do
@@ -18,7 +20,7 @@ for i in $(seq 0 4); do
         if [ ! -z "$CIRCLE_TAG" ]; then
             curl --header "Content-Type: application/json" \
                  --data "{\"build_parameters\": {\"CIRCLE_TAG\": \"$CIRCLE_TAG\", \"TOOLBOX_BUILD_NUM\": $CIRCLE_BUILD_NUM, \"TOOLBOX_ARTIFACTS\": \"$CIRCLE_ARTIFACTS\"}}" \
-                 --request POST "https://circleci.com/api/v1/project/docker/toolbox-release/tree/master?circle-token=$CIRCLE_TOKEN"
+                 --request POST "https://circleci.com/api/v1/project/docker/toolbox-release/tree/master?circle-token=$CIRCLE_TOKEN" || true
         fi
 
         exit 0
